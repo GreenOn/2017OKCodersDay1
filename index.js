b0faf44b602afde940b40992270307412fd56741
Pramod Vadrevu <pramodvadrevu@gmail.com>
2017-03-25 12:51:58 -0500

    adding index.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-Hello everone
-Hello everone
+var http = require('http');
+const PORT = 8080;
+function handleRequest(req, resp){
+	resp.end('<h1>Welcome to my Webpage');
+}
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+	console.log("Server is running on port: %s", PORT);
+});
