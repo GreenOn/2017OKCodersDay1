91273f1a3296a29586e446bebf1bceca9229f857
Pramod Vadrevu <pramodvadrevu@gmail.com>
2017-03-28 10:17:47 -0500

    changed port from const to var

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var http = require('http');
-const PORT = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 function handleRequest(req, resp){
 	resp.end('<h1>Welcome to my Webpage');
 }
 var server = http.createServer(handleRequest);
 
-server.listen(PORT, function(){
-	console.log("Server is running on port: %s", PORT);
+server.listen(port, function(){
+	console.log("Server is running on port: %s", port);
 });
